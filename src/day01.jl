d9f642cb38cd9af47030ea008b5bb12732a9fda2
Alexander Seiler <seileralex@gmail.com>
2021-12-01 07:01:37 +0100

    [Day 1] Simplify

--- src/day01.jl
+++ src/day01.jl
@@ -4,10 +4,8 @@ using AdventOfCode2021
 
 function day01(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
     reports = parse.(Int, split(input))
-    part1 = count(a > b for (a, b) ∈ zip(reports[2:end], reports[1:end-1]))
     sums = [a + b + c for (a, b, c) ∈ zip(reports[1:end-2], reports[2:end-1], reports[3:end])]
-    part2 = count(a > b for (a, b) ∈ zip(sums[2:end], sums[1:end-1]))
-    return [part1, part2]
+    return [count(diff(reports) .> 0), count(diff(sums) .> 0)]
 end
 
 end # module
