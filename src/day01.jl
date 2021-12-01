762facd16dbe7588ac66f7324c50e30f1adb88a3
Alexander Seiler <seileralex@gmail.com>
2021-12-01 06:30:26 +0100

    [Day 1] Add solution

--- src/day01.jl
+++ src/day01.jl
@@ -3,7 +3,11 @@ module Day01
 using AdventOfCode2021
 
 function day01(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
-    return input
+    reports = parse.(Int, split(input))
+    part1 = count(a > b for (a, b) ∈ zip(reports[2:end], reports[1:end-1]))
+    sums = [a + b + c for (a, b, c) ∈ zip(reports[1:end-2], reports[2:end-1], reports[3:end])]
+    part2 = count(a > b for (a, b) ∈ zip(sums[2:end], sums[1:end-1]))
+    return [part1, part2]
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using AdventOfCode2021
 using Test
 
 @testset "Day 1" begin
-    @test AdventOfCode2021.Day01.day01() == "Hello AdventOfCode2021!!\n"
+    @test AdventOfCode2021.Day01.day01() == [1527, 1575]
 end
