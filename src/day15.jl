0937a5704645f412247dce9ac25c10cea1a52b5c
Alexander Seiler <seileralex@gmail.com>
2021-12-16 01:32:31 +0100

    [Day 15] Add solution

---
+++ src/day15.jl
@@ -0,0 +1,36 @@
+module Day15
+
+using AdventOfCode2021
+using DataStructures
+
+function day15(input::String = readInput(joinpath(@__DIR__, "..", "data", "day15.txt")))
+    riskmap = reduce(vcat, permutedims.(map(x -> parse.(Int8, split(x, "")), split(input))))
+    p1 = solve(riskmap)
+    tmp = vcat((mod1.(riskmap .+ Int8(i), Int8(9)) for i=0:4)...)
+    riskmap = hcat((mod1.(tmp .+ Int8(i), Int8(9)) for i=0:4)...)
+    p2 = solve(riskmap)
+    return [p1, p2]
+end
+
+function solve(riskmap::Matrix{Int8})
+    costmap = fill(typemax(Int), size(riskmap)...)
+    costmap[1,1] = 0
+    visitedmap = zeros(Bool, size(riskmap)...)
+    Q = PriorityQueue{CartesianIndex{2}, Int}()
+    enqueue!(Q, CartesianIndex(1,1), 0)
+    while length(Q) > 0
+        current = dequeue!(Q)
+        visitedmap[current] = true
+        for neigh in (current + x for x in (CartesianIndex(1,0), CartesianIndex(-1, 0), CartesianIndex(0, 1), CartesianIndex(0, -1)))
+            (!checkbounds(Bool, riskmap, neigh) || visitedmap[neigh]) && continue
+            newcost = costmap[current] + riskmap[neigh]
+            if newcost < costmap[neigh]
+                costmap[neigh] = newcost
+                enqueue!(Q, neigh, newcost)
+            end
+        end
+    end
+    return costmap[end, end]
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -240,3 +240,18 @@ end
     @test AdventOfCode2021.Day14.day14(sample) == [1588, 2188189693529]
     @test AdventOfCode2021.Day14.day14() == [3406, 3941782230241]
 end
+
+@testset "Day 15" begin
+    sample = "1163751742\n" *
+             "1381373672\n" *
+             "2136511328\n" *
+             "3694931569\n" *
+             "7463417111\n" *
+             "1319128137\n" *
+             "1359912421\n" *
+             "3125421639\n" *
+             "1293138521\n" *
+             "2311944581\n"
+    @test AdventOfCode2021.Day15.day15(sample) == [40, 315]
+    @test AdventOfCode2021.Day15.day15() == [698, 3022]
+end
