33bf27bc953ebd44b32e7aa66eda673a2d857834
Alexander Seiler <seileralex@gmail.com>
2021-12-26 10:54:44 +0100

    [Day 20] Add solution

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ This Julia package contains my solutions for [Advent of Code 2021](https://adven
 | 17 | [:white_check_mark:](https://adventofcode.com/2021/day/17) | 1.032 ms | 364.98 KiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2021.jl/blob/master/src/day17.jl) |
 | 18 | [:white_check_mark:](https://adventofcode.com/2021/day/18) | 716.461 ms | 107.23 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2021.jl/blob/master/src/day18.jl) |
 | 19 | [:white_check_mark:](https://adventofcode.com/2021/day/19) | 250.784 ms | 301.46 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2021.jl/blob/master/src/day19.jl) |
+| 20 | [:white_check_mark:](https://adventofcode.com/2021/day/20) | 161.042 ms | 149.16 MiB | [:white_check_mark:](https://github.com/goggle/AdventOfCode2021.jl/blob/master/src/day20.jl) |
 
 
 The benchmarks have been measured on this machine:
---
+++ src/day20.jl
@@ -0,0 +1,50 @@
+module Day20
+
+using AdventOfCode2021
+
+function day20(input::String = readInput(joinpath(@__DIR__, "..", "data", "day20.txt")))
+    lines = split(input)
+    algorithm = map(x -> (x == "#" ? true : false), split(lines[1], ""))
+    image = map(x -> (x == '#' ? true : false), hcat(reduce.(vcat, lines[2:end])...))' |> collect
+
+    image, outsidepixelset = enhance(algorithm, image, false, 2)
+    p1 = image |> sum
+    p2 = enhance(algorithm, image, outsidepixelset, 48)[1] |> sum
+    return [p1, p2]
+
+end
+
+function enhance(algorithm, image, outsidepixelset, niter)
+    for _ = 1:niter
+        image, outsidepixelset = apply_algorithm(algorithm, image, outsidepixelset)
+    end
+    return image, outsidepixelset
+end
+
+function apply_algorithm(algorithm, image, outsidepixelset)
+    cimage = fill(outsidepixelset, size(image, 1) + 2, size(image, 2) + 2)
+    cimage[2:size(image, 1) + 1, 2:size(image, 2) + 1] = image
+    eimage = similar(cimage)
+    for i = 1:size(cimage, 1)
+        for j = 1:size(cimage, 2)
+            values = [2^l for l = 8:-1:0]
+            for m = i-1:i+1
+                for n = j-1:j+1
+                    if !checkbounds(Bool, cimage, m, n)
+                        pixel = outsidepixelset
+                    else
+                        pixel = cimage[m, n]
+                    end
+                    if !pixel
+                        values[3*(m-i+1) + n - j + 1 + 1] = 0
+                    end
+                end
+            end
+            eimage[i,j] = algorithm[(values |> sum) + 1]
+        end
+    end
+    outsidepixelset = (outsidepixelset ? algorithm[end] : algorithm[1])
+    return eimage, outsidepixelset
+end
+
+end # module
