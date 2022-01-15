c0ab10374d84285dfe5ba3eaa99da5682b164d5c
Alexander Seiler <seileralex@gmail.com>
2022-01-16 00:44:40 +0100

    [Day 21] Remove Memoize and use lookup table instead
    Using Memoize destroyed the ability to perform a benchmark.
    We could only measure the time to perform the lookup, which
    is not what we want...

--- src/day21.jl
+++ src/day21.jl
@@ -1,7 +1,6 @@
 module Day21
 
 using AdventOfCode2021
-using Memoize
 using StaticArrays
 
 function day21(input::String = readInput(joinpath(@__DIR__, "..", "data", "day21.txt")))
@@ -27,28 +26,29 @@ function part1(positions::Vector{Int})
 end
 
 function part2(positions::Vector{Int})
-    solve_part2(1, positions[1], positions[2], 0, 0, 1) |> maximum
+    solve_part2!(Dict{NTuple{5,Int8},MVector{2,Int}}(), Int8(1), Int8(positions[1]), Int8(positions[2]), Int8(0), Int8(0)) |> maximum
 end
 
-@memoize function solve_part2(pturn::Int, p1pos::Int, p2pos::Int, p1score::Int, p2score::Int, factor::Int)
+function solve_part2!(lookup::Dict{NTuple{5,Int8},MVector{2,Int}}, pturn::Int8, p1pos::Int8, p2pos::Int8, p1score::Int8, p2score::Int8)
     p1score >= 21 && return MVector(1, 0)
     p2score >= 21 && return MVector(0, 1)
-
-    ncomb = (1, 3, 6, 7, 6, 3, 1)
+    haskey(lookup, (pturn, p1pos, p2pos, p1score, p2score)) && return lookup[(pturn, p1pos, p2pos, p1score, p2score)]
+    ncomb = (Int8(1), Int8(3), Int8(6), Int8(7), Int8(6), Int8(3), Int8(1))
     wins = MVector(0, 0)
     if pturn == 1
-        for (dice, nc) ∈ zip(3:9, ncomb)
-            newp1pos = mod1(p1pos + dice, 10)
+        for (dice, nc) ∈ zip(UnitRange{Int8}(3:9), ncomb)
+            newp1pos = mod1(p1pos + dice, Int8(10))
             newp1score = p1score + newp1pos
-            wins .+= nc * solve_part2(2, newp1pos, p2pos, newp1score, p2score, factor * nc)
+            wins .+= nc * solve_part2!(lookup, Int8(2), newp1pos, p2pos, newp1score, p2score)
         end
     else
-        for (dice, nc) ∈ zip(3:9, ncomb)
-            newp2pos = mod1(p2pos + dice, 10)
+        for (dice, nc) ∈ zip(UnitRange{Int8}(3:9), ncomb)
+            newp2pos = mod1(p2pos + dice, Int8(10))
             newp2score = p2score + newp2pos
-            wins .+= nc * solve_part2(1, p1pos, newp2pos, p1score, newp2score, factor * nc)
+            wins .+= nc * solve_part2!(lookup, Int8(1), p1pos, newp2pos, p1score, newp2score)
         end
     end
+    lookup[(pturn, p1pos, p2pos, p1score, p2score)] = wins
     return wins
 end
 
