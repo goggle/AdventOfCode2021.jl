06a7323fbf8309738335763f05e5bfca9f8d65c8
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:16:50 +0200

    [Day 15] Format

--- src/day15.jl
+++ src/day15.jl
@@ -6,22 +6,22 @@ using DataStructures
 function day15(input::String = readInput(joinpath(@__DIR__, "..", "data", "day15.txt")))
     riskmap = reduce(vcat, permutedims.(map(x -> parse.(Int8, split(x, "")), split(input))))
     p1 = solve(riskmap)
-    tmp = vcat((mod1.(riskmap .+ Int8(i), Int8(9)) for i=0:4)...)
-    riskmap = hcat((mod1.(tmp .+ Int8(i), Int8(9)) for i=0:4)...)
+    tmp = vcat((mod1.(riskmap .+ Int8(i), Int8(9)) for i ∈ 0:4)...)
+    riskmap = hcat((mod1.(tmp .+ Int8(i), Int8(9)) for i ∈ 0:4)...)
     p2 = solve(riskmap)
     return [p1, p2]
 end
 
 function solve(riskmap::Matrix{Int8})
     costmap = fill(typemax(Int), size(riskmap)...)
-    costmap[1,1] = 0
+    costmap[1, 1] = 0
     visitedmap = zeros(Bool, size(riskmap)...)
     Q = PriorityQueue{CartesianIndex{2}, Int}()
-    enqueue!(Q, CartesianIndex(1,1), 0)
+    enqueue!(Q, CartesianIndex(1, 1), 0)
     while !isempty(Q)
         current = dequeue!(Q)
         visitedmap[current] = true
-        for neigh in (current + x for x in (CartesianIndex(1,0), CartesianIndex(-1, 0), CartesianIndex(0, 1), CartesianIndex(0, -1)))
+        for neigh in (current + x for x in (CartesianIndex(1, 0), CartesianIndex(-1, 0), CartesianIndex(0, 1), CartesianIndex(0, -1)))
             (!checkbounds(Bool, riskmap, neigh) || visitedmap[neigh]) && continue
             newcost = costmap[current] + riskmap[neigh]
             if newcost < costmap[neigh]
