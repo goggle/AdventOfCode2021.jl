20f087373ad8cfa82e12477a75705f60ef945001
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:14:00 +0200

    [Day 7] Format

--- src/day07.jl
+++ src/day07.jl
@@ -4,7 +4,7 @@ using AdventOfCode2021
 
 function day07(input::String = readInput(joinpath(@__DIR__, "..", "data", "day07.txt")))
     positions = parse.(Int, split(input, ','))
-    return [minimum(cost(positions, pos, part) for pos = minimum(positions):maximum(positions)) for part in (costp1, costp2)]
+    return [minimum(cost(positions, pos, part) for pos ∈ minimum(positions):maximum(positions)) for part in (costp1, costp2)]
 end
 
 function cost(positions::Vector{Int}, final::Int, costp)
