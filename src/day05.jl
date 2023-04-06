10785f66e5f99100e3bd8ee06d9be69918043b4a
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:13:35 +0200

    [Day 5] Format

--- src/day05.jl
+++ src/day05.jl
@@ -9,7 +9,7 @@ function day05(input::String = readInput(joinpath(@__DIR__, "..", "data", "day05
 end
 
 function parse_input(input::String)
-    starts, ends = Vector{Tuple{Int,Int}}(), Vector{Tuple{Int,Int}}()
+    starts, ends = Vector{Tuple{Int, Int}}(), Vector{Tuple{Int, Int}}()
     for line in split(rstrip(input), '\n')
         m = match(r"(\d+),(\d+)\s*->\s*(\d+),(\d+)", line)
         push!(starts, (parse(Int, m.captures[1]), parse(Int, m.captures[2])))
@@ -19,19 +19,19 @@ function parse_input(input::String)
 end
 
 function solve(starts, ends)
-    n, m = (maximum([maximum(a -> a[i], x) for x in (starts, ends)]) for i = 1:2)
-    grid = OffsetArray(zeros(UInt8, n+1, m+1), 0:n, 0:m)
-    gridp2 = OffsetArray(zeros(UInt8, n+1, m+1), 0:n, 0:m)
+    n, m = (maximum([maximum(a -> a[i], x) for x in (starts, ends)]) for i ∈ 1:2)
+    grid = OffsetArray(zeros(UInt8, n + 1, m + 1), 0:n, 0:m)
+    gridp2 = OffsetArray(zeros(UInt8, n + 1, m + 1), 0:n, 0:m)
     for (s, e) in zip(starts, ends)
         if s[1] == e[1]
             y1, y2 = minmax(s[2], e[2])
-            for y = y1:y2
+            for y ∈ y1:y2
                 grid[s[1], y] += 1
                 gridp2[s[1], y] += 1
             end
         elseif s[2] == e[2]
             x1, x2 = minmax(s[1], e[1])
-            for x = x1:x2
+            for x ∈ x1:x2
                 grid[x, s[2]] += 1
                 gridp2[x, s[2]] += 1
             end
@@ -39,7 +39,7 @@ function solve(starts, ends)
             xdir = s[1] < e[1] ? 1 : -1
             ydir = s[2] < e[2] ? 1 : -1
             x, y = s[1], s[2]
-            for i = 0:abs(s[1] - e[1])
+            for i ∈ 0:abs(s[1] - e[1])
                 gridp2[x, y] += 1
                 x += xdir
                 y += ydir
