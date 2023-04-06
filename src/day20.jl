9d7d25e84b31d610d7021c485f7f4206c30ed875
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:26:04 +0200

    [Day 20] Format

--- src/day20.jl
+++ src/day20.jl
@@ -14,7 +14,7 @@ function day20(input::String = readInput(joinpath(@__DIR__, "..", "data", "day20
 end
 
 function enhance(algorithm, image, outsidepixelset, niter)
-    for _ = 1:niter
+    for _ ∈ 1:niter
         image, outsidepixelset = apply_algorithm(algorithm, image, outsidepixelset)
     end
     return image, outsidepixelset
@@ -22,24 +22,24 @@ end
 
 function apply_algorithm(algorithm, image, outsidepixelset)
     cimage = fill(outsidepixelset, size(image, 1) + 2, size(image, 2) + 2)
-    cimage[2:size(image, 1) + 1, 2:size(image, 2) + 1] = image
+    cimage[2:size(image, 1)+1, 2:size(image, 2)+1] = image
     eimage = similar(cimage)
-    for i = 1:size(cimage, 1)
-        for j = 1:size(cimage, 2)
+    for i ∈ 1:size(cimage, 1)
+        for j ∈ 1:size(cimage, 2)
             values = MVector(256, 128, 64, 32, 16, 8, 4, 2, 1)
-            for m = i-1:i+1
-                for n = j-1:j+1
+            for m ∈ i-1:i+1
+                for n ∈ j-1:j+1
                     if !checkbounds(Bool, cimage, m, n)
                         pixel = outsidepixelset
                     else
                         pixel = cimage[m, n]
                     end
                     if !pixel
-                        values[3*(m-i+1) + n - j + 1 + 1] = 0
+                        values[3*(m-i+1)+n-j+1+1] = 0
                     end
                 end
             end
-            eimage[i,j] = algorithm[(values |> sum) + 1]
+            eimage[i, j] = algorithm[(values|>sum)+1]
         end
     end
     outsidepixelset = (outsidepixelset ? algorithm[end] : algorithm[1])
