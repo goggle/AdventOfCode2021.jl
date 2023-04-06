64936a671b9d2dd1fa975ba2f66c125093855ff8
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:27:23 +0200

    [Day 25] Format

--- src/day25.jl
+++ src/day25.jl
@@ -17,18 +17,18 @@ end
 function step!(watermap::Matrix{Char})
     changed = false
     move_right = findall(==('>'), watermap)
-    filter!(x -> watermap[x[1], mod1(x[2]+1, size(watermap,2))] == '.', move_right)
+    filter!(x -> watermap[x[1], mod1(x[2] + 1, size(watermap, 2))] == '.', move_right)
     for ci ∈ move_right
         changed = true
         watermap[ci] = '.'
-        watermap[ci[1], mod1(ci[2]+1, size(watermap,2))] = '>'
+        watermap[ci[1], mod1(ci[2] + 1, size(watermap, 2))] = '>'
     end
     move_down = findall(==('v'), watermap)
-    filter!(x -> watermap[mod1(x[1]+1, size(watermap,1)), x[2]] == '.', move_down)
+    filter!(x -> watermap[mod1(x[1] + 1, size(watermap, 1)), x[2]] == '.', move_down)
     for ci ∈ move_down
         changed = true
         watermap[ci] = '.'
-        watermap[mod1(ci[1]+1, size(watermap,1)), ci[2]] = 'v'
+        watermap[mod1(ci[1] + 1, size(watermap, 1)), ci[2]] = 'v'
     end
     return changed
 end
